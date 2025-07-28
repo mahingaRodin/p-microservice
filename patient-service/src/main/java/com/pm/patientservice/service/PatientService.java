250907ea5ba7107fbbee84acc3a4b45a52750891
P-Odiix <mahingarodin@gmail.com>
2025-07-28 02:11:25 +0200

    [creation of patients and exception handling]

--- patient-service/src/main/java/com/pm/patientservice/service/PatientService.java
+++ patient-service/src/main/java/com/pm/patientservice/service/PatientService.java
@@ -1,5 +1,6 @@
 package com.pm.patientservice.service;
 
+import com.pm.patientservice.dto.PatientRequestDTO;
 import com.pm.patientservice.dto.PatientResponseDTO;
 import com.pm.patientservice.mapper.PatientMapper;
 import com.pm.patientservice.model.Patient;
@@ -18,9 +19,16 @@ public class PatientService {
 
     public List<PatientResponseDTO> getPatients() {
         List<Patient> patients = patientRepository.findAll();
-
-        List<PatientResponseDTO> patientResponseDTOS = patients.stream()
+        return patients.stream()
                 .map(PatientMapper::toDto).toList();
-        return patientResponseDTOS;
     }
+
+    public PatientResponseDTO createPatient(PatientRequestDTO patientRequestDTO) {
+        Patient newPatient = patientRepository.save(
+                PatientMapper.toModel(patientRequestDTO)
+        );
+
+        return PatientMapper.toDto(newPatient);
+    }
+
 }
